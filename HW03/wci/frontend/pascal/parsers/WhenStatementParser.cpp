b3f2754a7e3e4aa9bee8473df8f583599a4d425f
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-09-30 11:27:55 -0700

    construction of nodes

--- HW03/wci/frontend/pascal/parsers/WhenStatementParser.cpp
+++ HW03/wci/frontend/pascal/parsers/WhenStatementParser.cpp
@@ -52,10 +52,10 @@ ICodeNode *WhenStatementParser::parse_statement(Token *token) throw (string)
     // TODO: I made current token call randomly because for some reasons errors were happening that
     // resulted in stack dumps
     token = next_token(token);  // consume the WHEN
-
+    vector<ICodeNode*> children;
     // no op node for testing
-    ICodeNode* nothing_node = ICodeFactory::create_icode_node((ICodeNodeType) NT_NO_OP);
-
+    ICodeNode* root_node = ICodeFactory::create_icode_node((ICodeNodeType) NT_WHEN);
+    ICodeNode* tmp_node;
     // text for each statemtn on the side of the =>
     string statement_text;
     ExpressionParser e(this);
@@ -63,45 +63,55 @@ ICodeNode *WhenStatementParser::parse_statement(Token *token) throw (string)
 
     // look through and prase all branches until otherwise keyword encountered
     while(token->get_text() != "OTHERWISE"){
-        parse_branch(token);
+        children.clear();
+        tmp_node = parse_branch(token);
+        children = tmp_node->get_children();
+        root_node->add_child(children[0]);
+        root_node->add_child(children[1]);
         token = current_token();
     }
 
     token = next_token(token); // consume otherwise
     if(token->get_text() != "=>") ;// throw error here
     token = next_token(token);
-    s.parse_statement(token);
+    root_node->add_child(s.parse_statement(token));
     token = current_token();
     token = next_token(token);
 
-    return nothing_node;
+    return root_node;
 }
 
 ICodeNode *WhenStatementParser::parse_branch(Token *token) throw (string)
 {
 
-    ICodeNode* nothing_node = ICodeFactory::create_icode_node((ICodeNodeType) NT_NO_OP);
+    ICodeNode* root_node = ICodeFactory::create_icode_node((ICodeNodeType) NT_COMPOUND);
+    ICodeNode* test_node = ICodeFactory::create_icode_node((ICodeNodeType) NT_TEST);
+    //ICodeNode* function_node = ICodeFactory::create_icode_node((ICodeNodeType) NT_COMPOUND);
+
     // Look for the : token.
     token = current_token();
 
     ExpressionParser e(this);
     StatementParser s(this);
-
-    e.parse_statement(token);
+    test_node->add_child(e.parse_statement(token));
 
     token = current_token();
     if(token->get_text() != "=>")
     {
         error_handler.flag(token, INVALID_CHARACTER, this); // temporary error should make specific for missing lambda
-        return nothing_node;
+        return root_node;
     }
     else token = next_token(token);
-    s.parse_statement(token);
+    //function_node->add_child(s.parse_statement(token));
+    root_node->add_child(test_node);
+    root_node->add_child(s.parse_statement(token));
 
     token = current_token();
     token = next_token(token); // skip semicolon
 
-    return nothing_node;
+
+
+    return root_node;
 }
 
 }}}}
