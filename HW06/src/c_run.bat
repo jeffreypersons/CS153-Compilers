d78eaeb55bb304a2b3bf4a9f15035f2bf7563e0c
nopenoway0 <benjamin.gottheil@sjsu.edu>
2017-11-16 20:18:36 -0800

    automated compile and run

--- HW06/src/c_run.bat
+++ HW06/src/c_run.bat
@@ -1,3 +1,2 @@
 javac -cp "./main;./gen/;../lib/antlr-4.7-complete.jar" simpLMain.java
-
-java -cp ".main;./gen/;.;../lib/antlr-4.7-complete.jar" simpLMain
+java -cp "./main;./gen/;.;../lib/antlr-4.7-complete.jar" simpLMain
