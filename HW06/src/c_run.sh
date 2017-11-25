8024f01d7588791577b903db24f1d6144e83b533
Ben <benjamin.gottheil@sjsu.edu>
2017-11-25 12:06:39 -0800

    fixed to run on unix

--- HW06/src/c_run.sh
+++ HW06/src/c_run.sh
@@ -1,2 +1,3 @@
-javac -cp "./main;./gen/;../lib/antlr-4.7-complete.jar" simpLMain.java
-java -cp "./main;./gen/;.;../lib/antlr-4.7-complete.jar" simpLMain test_program
+javac -cp "./main:./gen/:../lib/antlr-4.7-complete.jar" ./main/*.java
+javac -cp "./main:./gen/:../lib/antlr-4.7-complete.jar" simpLMain.java
+java -cp "./main:./gen/:.:../lib/antlr-4.7-complete.jar" simpLMain test_program
