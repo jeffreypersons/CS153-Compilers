2e4e9d60b6d1582fe182fd854fc9e1894623ce55
Jeffrey Persons <jperman8@gmail.com>
2017-11-26 12:18:46 -0800

    simpL: Optimized imports, changed testfile

--- HW06/src/simpLMain.java
+++ HW06/src/simpLMain.java
@@ -1,10 +1,9 @@
-import java.io.FileInputStream;
-import java.io.InputStream;
-import org.antlr.v4.runtime.tree.*;
 import java.io.PrintWriter;
-import java.io.FileWriter;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class simpLMain
@@ -13,12 +12,13 @@ public class simpLMain
 	{
         if(args.length == 0)
         { 
-            System.out.println("Improper arguments");
+            System.out.println("Invalid number of arguments");
+            System.out.println("Usage: java simpLMain");
             return;
         }
         String filename = args[0];
-        ArrayList<String> program = new ArrayList<String>();
-		simpLLexer lexer = new simpLLexer(CharStreams.fromFileName(filename));
+        ArrayList<String> program = new ArrayList<>();
+		simpLLexer lexer = new simpLLexer(CharStreams.fromPath(Paths.get(filename)));
     	simpLParser parser = new simpLParser( new CommonTokenStream( lexer ) );
     	parser.addParseListener(new simpLBaseListener());
     	ParseTree tree = parser.program();
@@ -36,7 +36,7 @@ public class simpLMain
         //System.out.println("necessary locals: " + visitor.getMemory().size() + "\n\t" + visitor.getMemory());
 
         // create program as list of statements/text
-       /** program.add(CodeEmitter.Program("test_program"));
+       /** program.add(CodeEmitter.Program("compile_test.txt"));
         program.add(CodeEmitter.SetStack(visitor.getStackSize()));
         program.add(CodeEmitter.EndMethod());
         for(String s : program)
@@ -45,7 +45,7 @@ public class simpLMain
         }
 
         //write output program here
-        PrintWriter s = new PrintWriter("test_program.j");
+        PrintWriter s = new PrintWriter("compile_test.txt.j");
         for(String str : program) s.write(str);
         s.close();*/
 	}
