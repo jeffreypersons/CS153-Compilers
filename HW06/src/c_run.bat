29aa848580e18e4b133f9ef20984ef50612efb80
Jeffrey Persons <jperman8@gmail.com>
2017-11-26 12:45:19 -0800

    Deleted older bare bones run scripts

--- HW06/src/c_run.bat
+++
@@ -1,3 +0,0 @@
-javac -cp "./main;./gen/;../lib/antlr-4.7-complete.jar" ./main/*.java
-javac -cp "./main;./gen/;../lib/antlr-4.7-complete.jar" simpLMain.java
-java -cp "./main;./gen/;.;../lib/antlr-4.7-complete.jar" simpLMain test_program
