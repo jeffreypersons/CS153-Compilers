297c654a456e5c47d163c1d9f9664091cd2af131
Jeffrey Persons <jperman8@gmail.com>
2017-11-26 12:26:53 -0800

    simpLMain: style tweaks + sys.exit

--- HW06/src/simpLMain.java
+++ HW06/src/simpLMain.java
@@ -8,29 +8,30 @@ import org.antlr.v4.runtime.tree.ParseTree;
 
 public class simpLMain
 {
-	public static void main(String[] args) throws Exception
-	{
-        if(args.length == 0)
-        { 
+    public static void main(String[] args) throws Exception
+    {
+        if (args.length == 0) {
             System.out.println("Invalid number of arguments");
-            System.out.println("Usage: java simpLMain");
-            return;
+            System.out.println("Usage: java simpLMain test.txt");
+            System.exit(1);
         }
         String filename = args[0];
-		simpLLexer lexer = new simpLLexer(CharStreams.fromPath(Paths.get(filename)));
-    	simpLParser parser = new simpLParser( new CommonTokenStream( lexer ) );
-    	parser.addParseListener(new simpLBaseListener());
-    	ParseTree tree = parser.program();
-    	cVisitor visitor = new cVisitor();
+
+        simpLLexer lexer = new simpLLexer(CharStreams.fromPath(Paths.get(filename)));
+        simpLParser parser = new simpLParser(new CommonTokenStream(lexer));
+        parser.addParseListener(new simpLBaseListener());
+
+        ParseTree tree = parser.program();
+        cVisitor visitor = new cVisitor();
 
         PrintWriter a = new PrintWriter(filename + ".j");
         a.write(CodeEmitter.Program(filename));
-    	visitor.visit(tree);
-        for(String str : visitor.getText())
+        visitor.visit(tree);
+        for (String str : visitor.getText())
         {
             a.write("\n" + str);
         }
-    	a.write("\nreturn\n" + CodeEmitter.EndMethod());
+        a.write("\nreturn\n" + CodeEmitter.EndMethod());
         a.close();
-	}
+    }
 }
