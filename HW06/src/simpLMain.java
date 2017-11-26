fa4fa573e63fcd167c6a4598a10dc85ceda043d3
Jeffrey Persons <jperman8@gmail.com>
2017-11-26 13:40:30 -0800

    simpLMain: fixed issue with input file extensions

--- HW06/src/simpLMain.java
+++ HW06/src/simpLMain.java
@@ -1,5 +1,5 @@
+import java.io.File;
 import java.io.PrintWriter;
-import java.nio.file.Paths;
 
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.CharStreams;
@@ -10,22 +10,23 @@ public class simpLMain
 {
     public static void main(String[] args) throws Exception
     {
-        if (args.length == 0) {
+        if (args.length == 0)
+        {
             System.out.println("Invalid number of arguments");
             System.out.println("Usage: java simpLMain test.txt");
             System.exit(1);
         }
-        String filename = args[0];
+        String baseFileName = getBaseName(args[0]);
 
-        simpLLexer lexer = new simpLLexer(CharStreams.fromPath(Paths.get(filename)));
+        simpLLexer lexer = new simpLLexer(CharStreams.fromFileName(baseFileName));
         simpLParser parser = new simpLParser(new CommonTokenStream(lexer));
         parser.addParseListener(new simpLBaseListener());
 
         ParseTree tree = parser.program();
         cVisitor visitor = new cVisitor();
 
-        PrintWriter a = new PrintWriter(filename + ".j");
-        a.write(CodeEmitter.Program(filename));
+        PrintWriter a = new PrintWriter(baseFileName + ".j");
+        a.write(CodeEmitter.Program(baseFileName));
         visitor.visit(tree);
         for (String str : visitor.getText())
         {
@@ -33,5 +34,23 @@ public class simpLMain
         }
         a.write("\nreturn\n" + CodeEmitter.EndMethod());
         a.close();
+    }
+    /**
+     * TODO: MOVE THIS TO A UTILS CLASS
+     * Return basename of given path, without extension or leading dot.
+     * For example:
+     *   ~/Users/.tmp => tmp, /.x.y.z/ => x, foo.tar.gz => foo
+     */
+    private static String getBaseName(String path)
+    {
+        String rawName = new File(path).getName();
+        if (rawName.equals("") || rawName.equals("."))
+            return "";
+
+        // ignoring starting dots, and end at the first dot found
+        int dotIndex = rawName.substring(1).indexOf('.');
+        int start = rawName.startsWith(".")? 1 : 0;
+        int end = dotIndex == -1? rawName.length() : dotIndex;
+        return rawName.substring(start, end);
     }
 }
