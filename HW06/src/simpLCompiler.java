5e8253a1c4f4d84312122b268bb26acb929cb914
Jeffrey Persons <jperman8@gmail.com>
2017-11-26 20:51:54 -0800

    Extracted a wrapper compiler class from main

--- HW06/src/simpLCompiler.java
+++ HW06/src/simpLCompiler.java
@@ -1,77 +1,63 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
 import gen.simpLBaseListener;
 import gen.simpLLexer;
 import gen.simpLParser;
 import main.CodeEmitter;
 import main.cVisitor;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.tree.ParseTree;
+import utils.FileUtils;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.PrintWriter;
+class SourceFileNotFoundException extends RuntimeException {}
 
-class SourceFileNotFoundException extends RuntimeException
-{
-
-}
-
+/**
+ * Client class for wrapping all the compiler components (lexer/parser/parseTree/symTabs/etc).
+ */
 public class simpLCompiler
 {
-    private simpLLexer lexer;
-    simpLParser parser;
+    // todo: changes can be made here in future, since backend won't need to change every time...
+    // but the lexer/parser wilL!
+    private final simpLLexer lexer;
+    private final simpLParser parser;
+    private final ParseTree parseTree;
+    private final cVisitor visitor;
+    private final String sourceFileName;
+    private final List<String> program = new ArrayList<>();
 
-    public simpLCompiler(String sourceFile)
+    public simpLCompiler(String sourceFileName)
     {
-        simpLLexer lexer = null;
         try
         {
-            lexer = new simpLLexer(CharStreams.fromFileName(sourceFile));
+            lexer = new simpLLexer(CharStreams.fromFileName(sourceFileName));
         }
         catch (IOException e)
         {
             throw new SourceFileNotFoundException();
         }
-
-        simpLParser parser = new simpLParser(new CommonTokenStream(lexer));
+        this.sourceFileName = sourceFileName;
+        parser = new simpLParser(new CommonTokenStream(lexer));
         parser.addParseListener(new simpLBaseListener());
 
-        ParseTree tree = parser.program();
-        cVisitor visitor = new cVisitor();
-
-        generateCode(sourceFile);
+        parseTree = parser.program();
+        visitor = new cVisitor();
     }
 
     /**
-     * Generate code for given SimpL code file.
+     * Generate jasmine code for given SimpL code file.
      */
-    private static void generateCode(String filename)
+    public void generateObjectCode()
     {
-        PrintWriter a = new PrintWriter(filename + ".j");
-        a.write(CodeEmitter.Program(filename));
-        visitor.visit(tree);
-        for (String str : visitor.getText())
-            a.write("\n" + str);
-        a.write("\nreturn\n" + CodeEmitter.EndMethod());
-        a.close();
-    }
+        // todo: require an extension like .sla or something, instead of the basename manipulation
+        String assemblyFileName = FileUtils.getBaseName(sourceFileName) + ".j";
 
-    /**
-     * TODO: MOVE THIS TO A UTILS CLASS
-     * Return basename of given path, without extension or leading dot.
-     * For example:
-     *   ~/Users/.tmp => tmp, /.x.y.z/ => x, foo.tar.gz => foo
-     */
-    private static String getBaseName(String path)
-    {
-        String rawName = new File(path).getName();
-        if (rawName.equals("") || rawName.equals("."))
-            return "";
-
-        // ignoring starting dots, and end at the first dot found
-        int dotIndex = rawName.substring(1).indexOf('.');
-        int start = rawName.startsWith(".")? 1 : 0;
-        int end = dotIndex == -1? rawName.length() : dotIndex + 1;
-        return rawName.substring(start, end);
+        visitor.visit(parseTree);
+        FileUtils.writeText(assemblyFileName, CodeEmitter.Program(sourceFileName));
+        FileUtils.writeLines(assemblyFileName, visitor.getText());
+        FileUtils.writeText(assemblyFileName, "\nreturn\n" + CodeEmitter.EndMethod());
     }
 }
--- HW06/src/simpLMain.java
+++ HW06/src/simpLMain.java
@@ -1,16 +1,4 @@
-import java.io.File;
-import java.io.PrintWriter;
-
-import gen.simpLBaseListener;
-import gen.simpLLexer;
-import gen.simpLParser;
-import main.CodeEmitter;
-import main.cVisitor;
-import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.tree.ParseTree;
-
-
+/** High level client class for running the SimpL Compiler. */
 public class simpLMain
 {
     public static void main(String[] args) throws Exception
@@ -21,42 +9,8 @@ public class simpLMain
             System.out.println("Usage: java simpLMain test.txt");
             System.exit(1);
         }
-        String baseFileName = getBaseName(args[0]);
-
-        simpLLexer lexer = new simpLLexer(CharStreams.fromFileName(baseFileName));
-        simpLParser parser = new simpLParser(new CommonTokenStream(lexer));
-        parser.addParseListener(new simpLBaseListener());
-
-        ParseTree tree = parser.program();
-        cVisitor visitor = new cVisitor();
-
-        PrintWriter a = new PrintWriter(baseFileName + ".j");
-        a.write(CodeEmitter.Program(baseFileName));
-        visitor.visit(tree);
-        for (String str : visitor.getText())
-        {
-            a.write("\n" + str);
-        }
-        a.write("\nreturn\n" + CodeEmitter.EndMethod());
-        a.close();
-    }
-
-    /**
-     * TODO: MOVE THIS TO A UTILS CLASS
-     * Return basename of given path, without extension or leading dot.
-     * For example:
-     *   ~/Users/.tmp => tmp, /.x.y.z/ => x, foo.tar.gz => foo
-     */
-    private static String getBaseName(String path)
-    {
-        String rawName = new File(path).getName();
-        if (rawName.equals("") || rawName.equals("."))
-            return "";
-
-        // ignoring starting dots, and end at the first dot found
-        int dotIndex = rawName.substring(1).indexOf('.');
-        int start = rawName.startsWith(".")? 1 : 0;
-        int end = dotIndex == -1? rawName.length() : dotIndex + 1;
-        return rawName.substring(start, end);
+        String fileName = args[0];
+        simpLCompiler simpLCompiler = new simpLCompiler(fileName);
+        simpLCompiler.generateObjectCode();
     }
 }
