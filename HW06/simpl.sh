7abeacc73576738afdb67ee9630179cf8c39cc91
Jeffrey Persons <jperman8@gmail.com>
2017-12-02 13:37:32 -0800

    simpl.sh: cleanup + error handling

--- HW06/simpl.sh
+++ HW06/simpl.sh
@@ -20,20 +20,33 @@ if [ $# -ne 1 ]; then
     exit 1
 fi
 filename=$(basename $1 | cut -d'.' -f-1)
-filetype=`$(echo $1 | cut -d'.' -f2-)`
-if [[ ${filetype} == simpl ]]; then
-    echo $(basename $1)
+filetype=$(echo $1 | cut -d'.' -f2-)
+if [[ ${filetype} != simpl ]]; then
+    echo "  **Invalid file type for file: $(basename $1)**"
     echo "  Only .simpl files can be executed as SimpL code"
     exit 1
 fi
 
-# create .simpl -> .j -> .class file and then run using Java
-output="$filename"
-echo "Generating $filename.simpl"
-java -cp "out:lib/antlr-4.7-complete.jar" SimpLMain "$filename.simpl"
+# ------ generate jasmin file from simpl file
+java -ea -cp "out:lib/antlr-4.7-complete.jar" SimpLMain "$filename.simpl"
+if [ $? == 0 ]; then
+    echo "Successfully generated object file: $filename.j"
+else
+    echo "Failed to successfully generate object file: $filename.j"; exit $?
+fi
 
-echo "Generating $filename.j"
+# ------ generate class file from jasmin file
 java -jar lib/jasmin-2.4-complete.jar "$filename.j"
+if [ $? == 0 ]; then
+    echo "Successfully generated class file: $filename.class"
+else
+    echo "Failed to successfully generate class file: $filename.class"; exit $?
+fi
 
-echo "Running $filename.class"
-java -cp "__simplcache" "$filename.class"
+# ------ run class file
+java "$filename.class"
+if [ $? == 0 ]; then
+    echo "Successfully ran class file for: $filename.simpl"
+else
+    echo "Failed to successfully run class file for: $filename.simpl"; exit $?
+fi
