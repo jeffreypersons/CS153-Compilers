55320d3e7642479cbe383f236b91e0c74cf49118
Jeffrey Persons <jperman8@gmail.com>
2017-12-02 03:05:55 -0800

    Scripts: Add initial test.sh template

---
+++ HW06/test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# ============================ Test SimpL Code Base ============================
+# Test SimpL from sources
+# example usage: ./simpL.sh tests/basic_compile.txt
+#
+# This script does the following steps:
+# (1) test simpl using all the test files in the tests/dir
+# ==============================================================================
+
+# todo: add functionality (look into antlr test rig tool as well...)
