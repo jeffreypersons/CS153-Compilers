c9244c3f8395d6c45e8bcd55454b7688399f7318
Jeffrey Persons <jperman8@gmail.com>
2017-12-04 01:53:32 -0800

    Updated test files + script input validation

--- HW06/test.sh
+++ HW06/test.sh
@@ -1,17 +1,33 @@
 #!/usr/bin/env bash
 # ============================ Test SimpL Code Base ============================
 # Test SimpL from sources
-# example usage: ./simpL.sh tests/basic_compile.txt
+# example usage: ./test.sh
 #
 # This script does the following steps:
 # (1) test simpl using all the test files in the tests/dir
 # ==============================================================================
 
 # todo: add functionality (look into antlr test rig tool as well...)
+
+# ensure working dir is HW06 and no arguments are given
+if [[ $(basename $(pwd)) != HW06 ]]; then
+    echo "**Error processing input for build.sh**"
+    echo "  build.sh can only be run with HW06 as the working directory"
+    exit 1
+fi
+if [ $# -ne 0 ]; then
+    echo "**Error processing input for build.sh**"
+    echo "  Invalid number of arguments"
+    echo "  Run as $ ./build.sh"
+    exit 1
+fi
+
 printf "\n--------------- Running tests/basic_compile1.simpl ----------------\n"
+rm -rf tests/basic_compile1.j; rm -rf tests/basic_compile1.class
 ./simpl.sh tests/basic_compile1.simpl
 
 printf "\n--------------- Running tests/basic_compile2.simpl ----------------\n"
+rm -rf tests/basic_compile2.j; rm -rf tests/basic_compile2.class
 ./simpl.sh tests/basic_compile2.simpl
 
 printf "\n"
