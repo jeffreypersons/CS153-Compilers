8b0da8bd01a21756a6b4b23c5d2fe309035d3c03
Jeffrey Persons <jperman8@gmail.com>
2017-12-05 17:48:57 -0800

    Value: Removed redundant public modifier
    - `public` modifier is redundant for interface methods, as they are `public` by default, so I removed the modifiers

--- HW06/src/main/Value.java
+++ HW06/src/main/Value.java
@@ -7,16 +7,16 @@ public interface Value<T>
      * Used to extract value from an object
      * @return value extracted
      */
-    public T getValue();
+    T getValue();
     /**
      * Used to set a value into an object
      * @param value new value for object
      */
-    public void setValue(T value);
+    void setValue(T value);
     /**
      * Type of object as a string
      * @return label of the object possible labels are currently
      * IDENTIFIER and NUMBER
      */
-    public String getType();
+    String getType();
 }
