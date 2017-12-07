ef183cfb3d61f0cffaa2cbbf17bd73f6ca8d8574
Jeffrey Persons <jperman8@gmail.com>
2017-12-07 12:12:35 -0800

    Main: Remove redundant input validation
    - Now that input validation is effectively done in the compiler constructor with exceptions, validation in main is no longer needed.

--- HW06/src/SimpLMain.java
+++ HW06/src/SimpLMain.java
@@ -1,4 +1,3 @@
-import utils.FileUtils;
 
 // todo: split up run compile functionality as command line option (via java program args)
 
@@ -8,7 +7,6 @@ public class SimpLMain
     /** Takes simpl filepath, and generates jasmin code in the same directory. */
     public static void main(String[] args)
     {
-        // todo: move these error messages to exceptions thrown in compiler class
         if (args.length != 1)
         {
             System.out.println("**Error processing SimpL input file**");
@@ -16,24 +14,9 @@ public class SimpLMain
             System.out.println("  Run as $ java SimpLMain <simpl_filepath>");
             System.exit(1);
         }
-        String simpFilepath = args[0];
-        if (!FileUtils.isFile(simpFilepath))
-        {
-            System.out.println("**Error processing SimpL input file**");
-            System.out.println("  Invalid file path " + FileUtils.getEntireFileExtension(simpFilepath));
-            System.out.println("  File does not exist");
-            System.exit(1);
-        }
-        if (!FileUtils.getEntireFileExtension(simpFilepath).equals(".simpl"))
-        {
-            System.out.println(FileUtils.getEntireFileExtension(simpFilepath));
-            System.out.println("**Error processing SimpL input file**");
-            System.out.println("  Invalid file extension for file " + FileUtils.getEntireFileExtension(simpFilepath));
-            System.out.println("  Only .simpl files are supported");
-            System.exit(1);
-        }
         System.out.println();
-        SimpLCompiler simpLCompiler = new SimpLCompiler(simpFilepath);
-        simpLCompiler.compile();
+
+        SimpLCompiler compiler = new SimpLCompiler(args[0]);
+        compiler.compile();
     }
 }
