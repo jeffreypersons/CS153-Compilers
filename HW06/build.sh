b7c380d53e51bb4fc9dbbb7b1d80950f12d9754f
Jeffrey Persons <jperman8@gmail.com>
2017-12-08 15:33:44 -0800

    Scripts: Consolidate comments + ensure hw06 is cwd
    - Greatly consolidated and updated comments
    - Since classpaths and paths are relative to the HW06 directory, ensured script input validation enforce that they are run from HW06 directory

--- HW06/build.sh
+++ HW06/build.sh
@@ -1,38 +1,27 @@
 #!/usr/bin/env bash
 # ========================= Generate and Build SimpL Sources ===================
-# Build SimpL from sources
+# Generate antlr sources and compile SimpL codebase from HW06 directory
 # Example usage: ./build.sh
-#
-# Requirements:
-# (1) HW06 is currently the only supported working directory
-# (2) No arguments are supported at this time
-#
-# This script does the following steps:
-# (1) Generates Java source code for listener, visitor, parser, and tokens
-#     using antlr4
-# (2) Compiles all of the source directory (generates .class files adjacent to
-#     .java file)
 # ==============================================================================
 
-# emulate realpath (not on all os) and then validate input
+# emulate realpath (its not available on all systems)
 realpath() { [[ $1 = /* ]] && echo $1 || echo "$(pwd)/${1#./}" | sed 's/\/*$//g'; }
-cwd=$(realpath)
-export CLASSPATH="lib/jasmin-2.4-complete.jar:lib/antlr-4.7-complete.jar"
 
 # ensure working dir is HW06 and no arguments are given
-if [[ $(basename $(pwd)) != HW06 ]]; then
+cwd=$(realpath)
+if [[ ${cwd} != */*HW06 ]]; then
     echo "**Error processing input for build.sh**"
     echo "  build.sh can only be run with HW06 as the working directory"
     exit 1
 fi
-if [ $# -ne 0 ]; then
+if [ ${#} -ne 0 ]; then
     echo "**Error processing input for build.sh**"
     echo "  Invalid number of arguments"
     echo "  Run as $ ./build.sh"
     exit 1
 fi
 
-# generate antlr sources using absolute paths to avoid tool conflicts
+# generate simpl listener/visitor/parser/tokens files, using full paths to avoid conflicts
 echo "Generating antlr4 source files into ./src/gen"
 java -jar lib/antlr-4.7-complete.jar \
       ${cwd}/src/SimpL.g4 \
@@ -42,9 +31,10 @@ java -jar lib/antlr-4.7-complete.jar \
      -package gen         \
      -o ${cwd}/src/gen    \
 
-# generate class files in out dir
+# compile entire SimpL compiler codebase with antlr4.7 and jasmin2.4 libraries
 echo "Compiling all Java files in ./src sources into ./out"
 rm -rf out; mkdir out
 find . -name '*.java' > out/sources.txt
+export CLASSPATH="lib/jasmin-2.4-complete.jar:lib/antlr-4.7-complete.jar"
 javac -cp lib/jasmin-2.4-complete.jar:lib/antlr-4.7-complete.jar \
       -d out @out/sources.txt \
--- HW06/simpl.sh
+++ HW06/simpl.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 # ============================= Execute SimpL File =============================
-# Run SimpL from sources
-# Example usage: ./simpL.sh tests/basic_compile.simpl
-# Note that only a single .simpl file can be compiled/run at a time
+# Compile and run a single SimpL file
+# Example usage: ./simpl.sh tests/basic_compile.simpl
 # ==============================================================================
 
-get_extension() { echo "."$(echo $(basename $1) | cut -d '.' -f2-); }
-
-filepath=${1}
-if [ $# -ne 1 ]; then
-    echo "**Error processing input for simpl.sh**"
+# ensure working dir is HW06 and no arguments are given
+cwd=$(realpath)
+if [[ ${cwd} != */*HW06 ]]; then
+    echo "**Error processing input for build.sh**"
+    echo "  simpl.sh can only be run with HW06 as the working directory"
+    exit 1
+fi
+if [ ${#} -ne 0 ]; then
+    echo "**Error processing input for build.sh**"
     echo "  Invalid number of arguments"
-    echo "  Run as $ ./simpl.sh <source_filepath>.simpl"
+    echo "  Run as $ ./simpl.sh"
     exit 1
 fi
 
-# run compiler with antlr4.7 and jasmin2.4 libraries
+# run SimpL compiler with antlr4.7 and jasmin2.4 libraries
 export CLASSPATH="out:lib/jasmin-2.4-complete.jar:lib/antlr-4.7-complete.jar"
 java -cp ${CLASSPATH} SimpLMain ${filepath}
