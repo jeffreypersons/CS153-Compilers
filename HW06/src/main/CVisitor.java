7b819b08c33385db50e390283846ca88c034792d
Jeffrey Persons <jperman8@gmail.com>
2017-12-15 23:13:47 -0800

    Moved ErrorMessages to utils
    - more logical in uml...

--- HW06/src/main/CVisitor.java
+++ HW06/src/main/CVisitor.java
@@ -14,6 +14,7 @@ import org.antlr.v4.runtime.Token;
 
 import gen.SimpLBaseVisitor;
 import gen.SimpLParser;
+import utils.ErrorMessages;
 
 public class CVisitor extends SimpLBaseVisitor<TerminalNode>
 {
--- HW06/src/utils/ErrorMessages.java
+++ HW06/src/utils/ErrorMessages.java
@@ -1,4 +1,4 @@
-package main;
+package utils;
 
 import gen.SimpLParser;
 
